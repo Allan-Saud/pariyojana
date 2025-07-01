bc49107178fddabbdfebba665d32400a186d9050
Allan-Saud <cruxheli1@gmail.com>
2025-07-01 16:15:30 +0545

    first installment done

--- pariyojana_backend/projects/pdfs/First_Installment/utils.py
+++ pariyojana_backend/projects/pdfs/First_Installment/utils.py
@@ -1,24 +1,3 @@
-# # projects/pdfs/first_installment/utils.py
-
-# from projects.models.project import Project
-
-# def build_pdf_context(serial_no, project_id):
-#     try:
-#         project = Project.objects.get(pk=project_id)
-
-#         context = {
-#             "project_name": project.project_name,
-#             "fiscal_year": project.fiscal_year.year if project.fiscal_year else "",
-#             "location": project.location or "-",
-#             "budget": f"{project.budget} रू",
-#             # You can add more context per serial_no
-#         }
-
-#         return context
-#     except Project.DoesNotExist:
-#         raise Http404("Project not found.")
-
-
 from projects.models.Consumer_Committee.official_detail import OfficialDetail
 from projects.models.project import Project  # or wherever your Project model lives
 
@@ -30,5 +9,6 @@ def build_pdf_context(serial_no, project_id):
         "serial_no": serial_no,
         "project": project,
         "officials": officials,
-        # ... any other values used in the template
+        "project_name":project.project_name
+    
     }
