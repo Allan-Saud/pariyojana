a581d3fee532f94fcaf5e2e6b9c8dc0785058822
Allan-Saud <cruxheli1@gmail.com>
2025-07-07 15:33:43 +0545

    done

--- pariyojana_backend/projects/serializers/project.py
+++ pariyojana_backend/projects/serializers/project.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from projects.models.project import Project
 
+# class ProjectSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Project
+#         fields = '__all__'
+#         read_only_fields = ['id', 'created_at', 'updated_at', 'deleted_at', 'deleted_by', 'is_deleted']
+
 class ProjectSerializer(serializers.ModelSerializer):
+    area_name = serializers.CharField(source='area.name', read_only=True)
+    sub_area_name = serializers.CharField(source='sub_area.name', read_only=True)
+    source_name = serializers.CharField(source='source.name', read_only=True)
+    expenditure_center_name = serializers.CharField(source='expenditure_center.name', read_only=True)
+    fiscal_year_name = serializers.CharField(source='fiscal_year.year', read_only=True)
+    unit_name = serializers.CharField(source='unit.name', read_only=True)
+    project_level_name = serializers.CharField(source='project_level.name', read_only=True)
+
     class Meta:
         model = Project
         fields = '__all__'
