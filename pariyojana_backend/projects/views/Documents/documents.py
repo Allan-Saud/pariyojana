0728f0aa99d8fbf6d705f114b3826ddbc041cf19
Allan-Saud <cruxheli1@gmail.com>
2025-07-10 22:07:00 +0545

    done

--- pariyojana_backend/projects/views/Documents/documents.py
+++ pariyojana_backend/projects/views/Documents/documents.py
@@ -1,11 +1,21 @@
 from rest_framework import viewsets, permissions
 from projects.models.Documents.documents import Document
 from projects.serializers.Documents.documents import DocumentSerializer
+from projects.models.project import Project
+from django.shortcuts import get_object_or_404
+
+# views.py
 
 class DocumentViewSet(viewsets.ModelViewSet):
-    queryset = Document.objects.all().order_by('-uploaded_at')
     serializer_class = DocumentSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def get_queryset(self):
+        serial_number = self.kwargs.get('serial_number')
+        return Document.objects.filter(project__serial_number=serial_number).order_by('-uploaded_at')
+
     def perform_create(self, serializer):
-        serializer.save(uploaded_by=self.request.user)
+        serial_number = self.kwargs.get('serial_number')
+        project = get_object_or_404(Project, serial_number=serial_number)
+        serializer.save(uploaded_by=self.request.user, project=project)
+
