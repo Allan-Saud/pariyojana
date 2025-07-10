e246b6f2159defe665a814707ddb289a44628f2f
Allan-Saud <cruxheli1@gmail.com>
2025-07-10 22:50:52 +0545

    done

--- pariyojana_backend/project_management/settings.py
+++ pariyojana_backend/project_management/settings.py
@@ -77,7 +77,7 @@ DEFAULT_FROM_EMAIL = 'saudallan99@gmail.com'
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),  # or more
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1), 
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
