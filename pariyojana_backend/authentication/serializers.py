a7eee949dff7b6849f7cdb6e633f0d1a119ed96d
Allan-Saud <cruxheli1@gmail.com>
2025-07-14 23:01:40 +0545

    done

--- pariyojana_backend/authentication/serializers.py
+++ pariyojana_backend/authentication/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from authentication.models import VerificationLog 
 
 class VerificationLogSerializer(serializers.ModelSerializer):
+    project_name = serializers.CharField(source='project.name', read_only=True)
     class Meta:
         model = VerificationLog
         fields = '__all__'
