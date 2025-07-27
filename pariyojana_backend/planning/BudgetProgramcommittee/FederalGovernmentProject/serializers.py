7d64bbb23cc35ea44f3a3e59f0560bd0634a1309
Allan-Saud <cruxheli1@gmail.com>
2025-07-27 16:10:14 +0545

    worked on planning serializers

--- pariyojana_backend/planning/BudgetProgramcommittee/FederalGovernmentProject/serializers.py
+++ pariyojana_backend/planning/BudgetProgramcommittee/FederalGovernmentProject/serializers.py
@@ -1,30 +1,149 @@
+# # from rest_framework import serializers
+# # from .models import BudgetProgramFederalGovernmentProgram
+
+# # class BudgetProgramFederalGovernmentProgramSerializer(serializers.ModelSerializer):
+# #     class Meta:
+# #         model = BudgetProgramFederalGovernmentProgram
+# #         fields = '__all__'
+
+
 # from rest_framework import serializers
 # from .models import BudgetProgramFederalGovernmentProgram
+# from project_settings.models.thematic_area import ThematicArea
+# from project_settings.models.sub_thematic_area import SubArea
+# from project_settings.models.source import Source
+# from project_settings.models.expenditure_center import ExpenditureCenter
+# from planning.PlanEntry.serializers import PlanEntrySerializer
 
 # class BudgetProgramFederalGovernmentProgramSerializer(serializers.ModelSerializer):
+#     thematic_area = serializers.SlugRelatedField(slug_field='name', queryset=ThematicArea.objects.all())
+#     sub_area = serializers.SlugRelatedField(slug_field='name', queryset=SubArea.objects.all())
+#     source = serializers.SlugRelatedField(slug_field='name', queryset=Source.objects.all())
+#     expenditure_center = serializers.SlugRelatedField(slug_field='name', queryset=ExpenditureCenter.objects.all())
+    
 #     class Meta:
 #         model = BudgetProgramFederalGovernmentProgram
 #         fields = '__all__'
-
-
+        
+        
 from rest_framework import serializers
 from .models import BudgetProgramFederalGovernmentProgram
 from project_settings.models.thematic_area import ThematicArea
 from project_settings.models.sub_thematic_area import SubArea
 from project_settings.models.source import Source
 from project_settings.models.expenditure_center import ExpenditureCenter
+from project_settings.models.project_level import ProjectLevel
+from project_settings.models.unit import Unit
+from project_settings.models.expenditure_title import ExpenditureTitle
+from project_settings.models.fiscal_year import FiscalYear
 from planning.PlanEntry.serializers import PlanEntrySerializer
 
+class SimpleIdNameSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+
 class BudgetProgramFederalGovernmentProgramSerializer(serializers.ModelSerializer):
-    thematic_area = serializers.SlugRelatedField(slug_field='name', queryset=ThematicArea.objects.all())
-    sub_area = serializers.SlugRelatedField(slug_field='name', queryset=SubArea.objects.all())
-    source = serializers.SlugRelatedField(slug_field='name', queryset=Source.objects.all())
-    expenditure_center = serializers.SlugRelatedField(slug_field='name', queryset=ExpenditureCenter.objects.all())
+    plan_entry = PlanEntrySerializer(read_only=True)
+    plan_entry_id = serializers.PrimaryKeyRelatedField(
+        queryset=PlanEntrySerializer.Meta.model.objects.all(),
+        source='plan_entry',
+        write_only=True,
+        allow_null=True,
+        required=False
+    )
+    
+    fiscal_year = SimpleIdNameSerializer(read_only=True)
+    fiscal_year_id = serializers.PrimaryKeyRelatedField(
+        queryset=FiscalYear.objects.all(),
+        source='fiscal_year',
+        write_only=True,
+        allow_null=True,
+        required=False
+    )
+
+    thematic_area = SimpleIdNameSerializer(read_only=True)
+    thematic_area_id = serializers.PrimaryKeyRelatedField(
+        queryset=ThematicArea.objects.all(),
+        source='thematic_area',
+        write_only=True
+    )
+
+    sub_area = SimpleIdNameSerializer(read_only=True)
+    sub_area_id = serializers.PrimaryKeyRelatedField(
+        queryset=SubArea.objects.all(),
+        source='sub_area',
+        write_only=True
+    )
+
+    project_level = SimpleIdNameSerializer(read_only=True)
+    project_level_id = serializers.PrimaryKeyRelatedField(
+        queryset=ProjectLevel.objects.all(),
+        source='project_level',
+        write_only=True,
+        allow_null=True,
+        required=False
+    )
+
+    expenditure_title = SimpleIdNameSerializer(read_only=True)
+    expenditure_title_id = serializers.PrimaryKeyRelatedField(
+        queryset=ExpenditureTitle.objects.all(),
+        source='expenditure_title',
+        write_only=True,
+        allow_null=True,
+        required=False
+    )
+
+    expenditure_center = SimpleIdNameSerializer(read_only=True)
+    expenditure_center_id = serializers.PrimaryKeyRelatedField(
+        queryset=ExpenditureCenter.objects.all(),
+        source='expenditure_center',
+        write_only=True
+    )
+
+    source = SimpleIdNameSerializer(read_only=True)
+    source_id = serializers.PrimaryKeyRelatedField(
+        queryset=Source.objects.all(),
+        source='source',
+        write_only=True
+    )
+
+    unit = SimpleIdNameSerializer(read_only=True)
+    unit_id = serializers.PrimaryKeyRelatedField(
+        queryset=Unit.objects.all(),
+        source='unit',
+        write_only=True,
+        allow_null=True,
+        required=False
+    )
     
     class Meta:
         model = BudgetProgramFederalGovernmentProgram
-        fields = '__all__'
-        
-        
-        
-        
+        fields = [
+            'id',
+            'plan_entry', 'plan_entry_id',
+            'fiscal_year', 'fiscal_year_id',
+            'plan_name',
+            'thematic_area', 'thematic_area_id',
+            'sub_area', 'sub_area_id',
+            'project_level', 'project_level_id',
+            'expenditure_title', 'expenditure_title_id',
+            'expenditure_center', 'expenditure_center_id',
+            'source', 'source_id',
+            'unit', 'unit_id',
+            'budget',
+            'ward_no',
+            'gps_coordinate',
+            'location',
+            'expected_result',
+            'feasibility_study',
+            'feasibility_file',
+            'detailed_study',
+            'detailed_file',
+            'environmental_study',
+            'environmental_file',
+            'status',
+            'priority_no',
+            'remarks',
+            'is_deleted',
+            'deleted_at',
+        ]
--- pariyojana_backend/planning/WardOffice/WardLevelProject/serializers.py
+++ pariyojana_backend/planning/WardOffice/WardLevelProject/serializers.py
@@ -5,35 +5,64 @@ from project_settings.models.sub_thematic_area import SubArea
 from project_settings.models.source import Source
 from project_settings.models.unit import Unit
 from project_settings.models.project_level import ProjectLevel
+from project_settings.models.expenditure_center import ExpenditureCenter
+from project_settings.models.expenditure_title import ExpenditureTitle
 
-from project_settings.models.expenditure_center import ExpenditureCenter   
-     
+class SimpleIdNameSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
 
 class WardLevelProjectSerializer(serializers.ModelSerializer):
-    thematic_area = serializers.SlugRelatedField(slug_field='name', queryset=ThematicArea.objects.all())
-    sub_area = serializers.SlugRelatedField(slug_field='name', queryset=SubArea.objects.all())
-    source = serializers.SlugRelatedField(slug_field='name', queryset=Source.objects.all())
-    unit = serializers.SlugRelatedField(slug_field='name', queryset=Unit.objects.all())
-    project_level = serializers.SlugRelatedField(slug_field='name', queryset=ProjectLevel.objects.all())
-    expenditure_center = serializers.SlugRelatedField(slug_field='name', queryset=ExpenditureCenter.objects.all())
-    
+    thematic_area = SimpleIdNameSerializer(read_only=True)
+    thematic_area_id = serializers.PrimaryKeyRelatedField(
+        queryset=ThematicArea.objects.all(), source='thematic_area', write_only=True
+    )
+
+    sub_area = SimpleIdNameSerializer(read_only=True)
+    sub_area_id = serializers.PrimaryKeyRelatedField(
+        queryset=SubArea.objects.all(), source='sub_area', write_only=True
+    )
+
+    source = SimpleIdNameSerializer(read_only=True)
+    source_id = serializers.PrimaryKeyRelatedField(
+        queryset=Source.objects.all(), source='source', write_only=True
+    )
+
+    unit = SimpleIdNameSerializer(read_only=True)
+    unit_id = serializers.PrimaryKeyRelatedField(
+        queryset=Unit.objects.all(), source='unit', write_only=True, allow_null=True, required=False
+    )
+
+    project_level = SimpleIdNameSerializer(read_only=True)
+    project_level_id = serializers.PrimaryKeyRelatedField(
+        queryset=ProjectLevel.objects.all(), source='project_level', write_only=True, allow_null=True, required=False
+    )
+
+    expenditure_center = SimpleIdNameSerializer(read_only=True)
+    expenditure_center_id = serializers.PrimaryKeyRelatedField(
+        queryset=ExpenditureCenter.objects.all(), source='expenditure_center', write_only=True
+    )
+
+    expenditure_title = SimpleIdNameSerializer(read_only=True)
+    expenditure_title_id = serializers.PrimaryKeyRelatedField(
+        queryset=ExpenditureTitle.objects.all(), source='expenditure_title', write_only=True, allow_null=True, required=False
+    )
+
+
     class Meta:
         model = WardLevelProject
-        fields = '__all__'
+        fields = [
+            'id', 'plan_name', 
+            'thematic_area', 'thematic_area_id',
+            'sub_area', 'sub_area_id',
+            'source', 'source_id',
+            'expenditure_center', 'expenditure_center_id',
+            'expenditure_title', 'expenditure_title_id',
+            'budget', 'ward_no', 'gps_coordinate', 'expected_result', 'unit', 'unit_id',
+            'project_level', 'project_level_id', 'location', 'feasibility_study', 'feasibility_file',
+            'detailed_study', 'detailed_file', 'environmental_study', 'environmental_file',
+            'status', 'priority_no', 'remarks', 'is_deleted', 'deleted_at'
+        ]
         extra_kwargs = {
             'priority_no': {'required': False}
         }
-        
-        
-# from rest_framework import serializers
-# from .models import WardLevelProject
-# from planning.PlanEntry.models import PlanEntry
-# from planning.PlanEntry.serializers import PlanEntrySerializer  # 👈 import it
-
-# class WardLevelProjectSerializer(serializers.ModelSerializer):
-#     plan_entry = PlanEntrySerializer(read_only=True)  # 👈 nested serializer
-
-#     class Meta:
-#         model = WardLevelProject
-#         fields = '__all__'
-
