38bcd92fca39b96b86dd855348d7682290555a5a
Allan-Saud <cruxheli1@gmail.com>
2025-08-07 22:17:54 +0545

    Removed pariyojana-frontend from version control

--- pariyojana-frontend/src/types/projectType.ts
+++
@@ -1,9 +0,0 @@
-export type ProjectType = 
-  | "municipality-pride"
-  | "ward"
-  | "municipality"
-  | "thematic"
-  | "ward_thematic"
-  | "provience-transfer-projects"
-  | "federal-gov-projects"
-  | "budget-committee";
