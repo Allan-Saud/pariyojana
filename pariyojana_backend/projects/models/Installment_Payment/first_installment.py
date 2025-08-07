5d134b680534f37c21be04b6a2ae7364ec9aea0a
Allan-Saud <cruxheli1@gmail.com>
2025-08-07 16:17:42 +0545

    arranged serial number

--- pariyojana_backend/projects/models/Installment_Payment/first_installment.py
+++ pariyojana_backend/projects/models/Installment_Payment/first_installment.py
@@ -3,10 +3,13 @@ from django.db import models
 from projects.models.project import   Project
 class FirstInstallmentUpload(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='first_installments',null=True)
-    serial_no = models.PositiveIntegerField(unique=True)
+    serial_no = models.PositiveIntegerField()
     file = models.FileField(upload_to='first_installment_uploads/')
     remarks = models.TextField(blank=True, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        unique_together = ('project', 'serial_no')
+    
     def __str__(self):
-        return f"First Installment - Serial {self.serial_no}"
+        return f"First Installment - Serial {self.serial_no} for Project {self.project_id}"
